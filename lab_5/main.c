1d850449b52424d66f0d32260527274c226f4c9f
ctrlcandy <alexandraleshanu@yandex.ru>
2021-12-16 00:00:36 +0300

    lab5 was added

---
+++ lab_5/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex;
+int counter = 0;
+
+void *to_write() {
+    while ('^') {
+        pthread_mutex_lock(&mutex);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
+    }
+}
+
+void *to_read() {
+    while ('3') {
+        pthread_mutex_lock(&mutex);
+        printf("tid: %lu counter: %d\n", pthread_self(), counter);
+        pthread_mutex_unlock(&mutex);
+        sleep(2);
+    }
+}
+
+int main(int argc, char **argv) {
+    pthread_t read_threads[10];
+    pthread_t write_thread;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_create(&write_thread, NULL, to_write, NULL);
+
+    for (int i = 0; i < 10; ++i)
+        pthread_create(&read_threads[i], NULL, to_read, NULL);
+
+    pthread_join(write_thread, NULL);
+}
